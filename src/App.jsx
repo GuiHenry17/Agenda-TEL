8b6fe7d2615db60b39dd6cfc4b9ac7c70d74b63a
GuiHenry17 <gui201629@gmail.com>
2024-03-05 12:24:40 -0300

    AULA REMOTA 05/03

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+
+import "./global.css"
+
 export default function App(){
   return (
-    <main>
+    <div>
       <Header/>
       <Footer/>
-    </main>
+    </div>
   );
 
   
